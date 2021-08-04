f207ecf677d192e76673b01fefaaad151fe3d820
jgiroso <jrgiroso@gmail.com>
2021-08-04 16:20:06 -0400

    added MainApp and Potions class

---
+++ src/main/java/MainApplication.java
@@ -0,0 +1,111 @@
+
+import com.mysql.cj.jdbc.Driver;
+import daos.PokemonRepository;
+import models.Pokemon;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.StringJoiner;
+
+/**
+ * @author git-leon
+ * @version 1.0.0
+ * @date 8/2/21 9:49 AM
+ */
+public class MainApplication {
+
+    public static void main(String[] args) {
+        registerJDBCDriver();
+        Connection mysqlDbConnection = getConnection("mysql");
+        PokemonRepository pokemonRepository = new PokemonRepository(mysqlDbConnection);
+        executeStatement(mysqlDbConnection, "DROP DATABASE IF EXISTS databaseName;");
+        executeStatement(mysqlDbConnection, "CREATE DATABASE IF NOT EXISTS databaseName;");
+        executeStatement(mysqlDbConnection, "USE databaseName;");
+        executeStatement(mysqlDbConnection, new StringBuilder()
+                .append("CREATE TABLE IF NOT EXISTS databaseName.pokemonTable(")
+                .append("id int auto_increment primary key,")
+                .append("name text not null,")
+                .append("primary_type int not null,")
+                .append("secondary_type int null);")
+                .toString());
+
+        pokemonRepository.create(new Pokemon(12L, "Ivysaur", 3, 7));
+        pokemonRepository.create(new Pokemon(13L, "Ivysaurr", 3, 7));
+        System.out.println(pokemonRepository.readAll());
+
+    }
+
+    static ResultSet executeQuery(Connection connection, String sqlQuery) {
+        try {
+            Statement statement = getScrollableStatement(connection);
+            return statement.executeQuery(sqlQuery);
+        } catch (SQLException e) {
+            throw new Error(e);
+        }
+    }
+
+    static void printResults(ResultSet resultSet) {
+        try {
+            for (int rowNumber = 0; resultSet.next(); rowNumber++) {
+                String firstColumnData = resultSet.getString(1);
+                String secondColumnData = resultSet.getString(2);
+                String thirdColumnData = resultSet.getString(3);
+                System.out.println(new StringJoiner("\n")
+                        .add("Row number = " + rowNumber)
+                        .add("First Column = " + firstColumnData)
+                        .add("Second Column = " + secondColumnData)
+                        .add("Third column = " + thirdColumnData));
+            }
+        } catch (SQLException e) {
+            throw new Error(e);
+        }
+    }
+
+    static void executeStatement(Connection connection, String sqlStatement) {
+        try {
+            Statement statement = getScrollableStatement(connection);
+            statement.execute(sqlStatement);
+        } catch (SQLException e) {
+            throw new Error(e);
+        }
+    }
+
+    static Statement getScrollableStatement(Connection connection) {
+        int resultSetType = ResultSet.TYPE_SCROLL_INSENSITIVE;
+        int resultSetConcurrency = ResultSet.CONCUR_READ_ONLY;
+        try { // scrollable statements can be iterated more than once without closing
+            return connection.createStatement(resultSetType, resultSetConcurrency);
+        } catch (SQLException e) {
+            throw new Error(e);
+        }
+    }
+
+    static Connection getConnection(String dbVendor) {
+        String username = "root";
+        String password = "";
+        String url = new StringBuilder()
+                .append("jdbc:")
+                .append(dbVendor)
+                .append("://127.0.0.1/")
+                .append("?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC")
+                .toString();
+        try {
+            return DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            throw new Error(e);
+        }
+    }
+
+    static void registerJDBCDriver() {
+        // Attempt to register JDBC Driver
+        try {
+            DriverManager.registerDriver(Driver.class.newInstance());
+        } catch (InstantiationException | IllegalAccessException | SQLException e1) {
+            throw new RuntimeException(e1);
+        }
+    }
+
+}
---
+++ src/main/java/models/PotionPouch.java
@@ -0,0 +1,78 @@
+package models;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class PotionPouch {
+
+    private Long id;
+    private String name;
+    private String ingredient1;
+    private String ingredient2;
+    private String effect;
+
+    public PotionPouch() {}
+
+    public PotionPouch(Long id, String name, String ingredient1, String ingredient2, String effect) {
+        this.id = id;
+        this.name = name;
+        this.ingredient1 = ingredient1;
+        this.ingredient2 = ingredient2;
+        this.effect = effect;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIngredient1() {
+        return ingredient1;
+    }
+
+    public void setIngredient1(String ingredient1) {
+        this.ingredient1 = ingredient1;
+    }
+
+    public String getIngredient2() {
+        return ingredient2;
+    }
+
+    public void setIngredient2(String ingredient2) {
+        this.ingredient2 = ingredient2;
+    }
+
+    public String getEffect() {
+        return effect;
+    }
+
+    public void setEffect(String effect) {
+        this.effect = effect;
+    }
+
+    @Override
+    public String toString() {
+        try {
+            return new ObjectMapper().writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            return "PotionPouch {" +
+                    "ID = " + id +
+                    ", Name = " + name +
+                    ", First Ingredient = " + ingredient1 +
+                    ", Second Ingredient = " + ingredient2 +
+                    ", Effects = " + effect +
+                    "}";
+        }
+    }
+}
