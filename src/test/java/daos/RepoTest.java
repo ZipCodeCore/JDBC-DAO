2a2b2c3691b38983133638c25564de328a014a34
MannyMb <emmanuelnefo98@gmail.com>
2021-08-05 20:34:29 -0400

    fin

---
+++ src/test/java/daos/RepoTest.java
@@ -0,0 +1,26 @@
+package daos;
+
+import org.junit.Test;
+
+public class RepoTest {
+    @Test
+    public void testCreate() {
+
+    }
+
+    @Test
+    public void testFindAll() {
+
+    }
+
+    @Test
+    public void testUpdate() {
+
+    }
+
+    @Test
+    public void testDelete() {
+
+    }
+
+}
